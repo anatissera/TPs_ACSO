3c44d417f2cefbe75bb9f5fe07d3dd539359cf02
anatissera <atissera@udesa.edu.ar>
2025-03-19 16:07:17 -0300

    qué hacemo

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -8,4 +8,13 @@ void process_instruction()
      * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
      * access memory. 
      * */
+
 }
+// una función para decode y otra para excecute
+/* 1. Ejecutar la instrucción
+2. decodificarla para saber la operación y los operandos a utilizar
+3. realizar la operación
+4. Actualizar el PC con la proxima instrucción a ejecutar
+*/
+
+// IMPORTANTE: utiliza little endian
