698ea0a5ff9571a01dda1ea74e7d3d65fbdd536c
anatissera <atissera@udesa.edu.ar>
2025-03-20 11:15:03 -0300

    cambio de lugar

--- TP1-ARM/src/utils/logical_operations.c
+++ TP1-ARM/src/utils/logical_operations.c
@@ -7,3 +7,20 @@ EOR (Shifted Register)
 
 ORR (Shifted Register)
 */
+
+/*
+logicalOp(char * restOfInstruction, int operation, bool updateFlags, bool shifted);
+¿Qué hace?
+Ejecuta operaciones lógicas entre registros (AND, ORR, EOR).
+Puede actualizar los flags si updateFlags es true.
+Parámetro operation:
+0 → AND
+1 → ORR
+2 → EOR
+Cuándo se usa:
+Para AND(S), ORR(S) y EOR(S), con o sin actualización de flags.
+Soporta operandos normales o registros desplazados.
+Ejemplos de uso:
+logicalOp(instr, 0, true, true); → ANDS X1, X2, X3, LSL #1
+logicalOp(instr, 2, false, false); → EOR X1, X2, X3
+*/
--- TP1-ARM/src/utils/math_operations.c
+++ TP1-ARM/src/utils/math_operations.c
@@ -44,21 +44,6 @@ sub(instr, true, true, false); → SUBS X1, X2, X3, LSR #4
 sub(instr, true, false, true); → CMP X1, #42
 
 
-logicalOp(char * restOfInstruction, int operation, bool updateFlags, bool shifted);
-¿Qué hace?
-Ejecuta operaciones lógicas entre registros (AND, ORR, EOR).
-Puede actualizar los flags si updateFlags es true.
-Parámetro operation:
-0 → AND
-1 → ORR
-2 → EOR
-Cuándo se usa:
-Para AND(S), ORR(S) y EOR(S), con o sin actualización de flags.
-Soporta operandos normales o registros desplazados.
-Ejemplos de uso:
-logicalOp(instr, 0, true, true); → ANDS X1, X2, X3, LSL #1
-logicalOp(instr, 2, false, false); → EOR X1, X2, X3
-
 
 mul(char * restOfInstruction);
 ¿Qué hace?
