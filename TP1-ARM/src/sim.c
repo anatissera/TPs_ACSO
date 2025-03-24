7b19e5c1bbbf0502b0cb0f17cac485d6258e4ece
naomicouriel <71277206+naomicouriel@users.noreply.github.com>
2025-03-24 16:03:03 -0300

    See description
    Tenía estos cambios en stashed no lo llego a revisar porque me tengo que ir a llevar la compu al service pero lo subo para no perderlo
    Byeeeee

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -32,3 +32,18 @@ void process_instruction()
 // IMPORTANTE: asume que la memoria está en LITTLE ENDIAN
 
 
+Instruction decode_instruction(uint32_t instruction)
+{
+    Instruction decoded;
+    decoded.opcode = (instruction >> 26) & 0x3F;
+    decoded.operand1 = (instruction >> 16) & 0xFF;
+    decoded.operand2 = (instruction >> 0) & 0xFFFF;
+}
+
+Instruction execute_instruction(Instruction decoded)
+{
+    Instruction executed;
+    executed.opcode = decoded.opcode;
+    executed.operand1 = decoded.operand1;
+    executed.operand2 = decoded.operand2;
+}
