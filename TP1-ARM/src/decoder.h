8ee8f9148e24e71246b462360c14375efe2b54c7
anatissera <atissera@udesa.edu.ar>
2025-03-26 11:51:17 -0300

    agregué el decoder a ver si anda
    hay que ir descomentando las opcode entrys en execute de sim.c a medida que vayamos terminando las funciones.
    fijate que les puse nombres según cómo aparece pero si haces más de una en una misma función después en sim.c le pones el mismo nombre a todo antes de compilar. Por ejemplo yo había puesto 3 funciones para load ldur, ldurh, ldurb, después la terminé implementando en una sola Ldur_h_b, entonces fui a execute y cambié en donde decía ldur o ldurh o ldurb por Ldur_h_b.

---
+++ TP1-ARM/src/decoder.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+#include "shell.h"
+
+uint32_t decode(uint32_t instruction);
--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -2,6 +2,7 @@
 
 #include "shell.h"
 
+#include "decoder.h"
 #include "utils/additionals.h"
 #include "utils/branches.h"
 #include "utils/load_n_store.h"
@@ -10,25 +11,24 @@
 #include "utils/math_operations.h"
 
 
-
 // void Subs_Cmp_extended_reg(uint32_t instruction);
 // void Subs_Cmp_imm(uint32_t instruction);
 
 void execute(uint32_t instruction) {
   Opcode_Entry opcodes[] = {
-      {0xFFC00000, 0b10101011000, Adds_extended_reg},  
-      {0xFFFFFC00, 0b10110001, Adds_imm},             
-      {0xFFC00000, 0b11101011000, Subs_Cmp_extended_reg}, 
-      {0xFFFFFC00, 0b11110001, Subs_Cmp_imm},
-      {0xFFFFFC00, 0b11010100, HLT},
-      {0xFFFFFC00, 0b110100101, Movz},
-      {0xFFFFFC00, 0b11101010, Ands_shifted_reg},
-      {0xFFFFFC00, 0b11001010, Eor_shifted_reg},
-      {0xFFFFFC00, 0b10101010, Orr_shifted_reg},
-      {0xFC000000, 0b000101, B},                   
-      {0xFFFFFFFF, 0b1101011000111111000000, Br},  
-      {0xFFFFFC00, 0b01010100, B_cond},
-      {0xFFFFFC00, 0b110100110, lsl_lsr_imm},
+      // {0xFFC00000, 0b10101011000, Adds_extended_reg},  
+      // {0xFFFFFC00, 0b10110001, Adds_imm},             
+      // {0xFFC00000, 0b11101011000, Subs_Cmp_extended_reg}, 
+      // {0xFFFFFC00, 0b11110001, Subs_Cmp_imm},
+      // {0xFFFFFC00, 0b11010100, HLT},
+      // {0xFFFFFC00, 0b110100101, Movz},
+      // {0xFFFFFC00, 0b11101010, Ands_shifted_reg},
+      // {0xFFFFFC00, 0b11001010, Eor_shifted_reg},
+      // {0xFFFFFC00, 0b10101010, Orr_shifted_reg},
+      // {0xFC000000, 0b000101, B},                   
+      // {0xFFFFFFFF, 0b1101011000111111000000, Br},  
+      // {0xFFFFFC00, 0b01010100, B_cond},
+      // {0xFFFFFC00, 0b110100110, lsl_lsr_imm},
       {0xFFC00000, 0b11111000000, Stur_h_b},
       {0xFFC00000, 0b00111000000, Stur_h_b},
       {0xFFC00000, 0b01111000000, Stur_h_b},
@@ -36,11 +36,11 @@ void execute(uint32_t instruction) {
       {0xFFC00000, 0b00111000010, Ldur_h_b},
       {0xFFC00000, 0b01111000010, Ldur_h_b},
 
-      {0xFFC00000, 0b10001011000, Add_extended_reg},
-      {0xFFFFFC00, 0b10010001, Add_immediate},
-      {0xFFC00000, 0b10011011000, Mul},
-      {0xFFFFFC00, 0b10110100, Cbz},
-      {0xFFFFFC00, 0b10110101, Cbnz}
+      // {0xFFC00000, 0b10001011000, Add_extended_reg},
+      // {0xFFFFFC00, 0b10010001, Add_immediate},
+      // {0xFFC00000, 0b10011011000, Mul},
+      // {0xFFFFFC00, 0b10110100, Cbz},
+      // {0xFFFFFC00, 0b10110101, Cbnz}
   };
 
   int num_opcodes = sizeof(opcodes) / sizeof(opcodes[0]);
@@ -56,15 +56,20 @@ void execute(uint32_t instruction) {
 }
 
 void process_instruction() {
-  uint32_t instruction = mem_read_32(CURRENT_STATE.PC); 
+  uint32_t instruction = mem_read_32(CURRENT_STATE.PC);
   uint32_t opcode = decode(instruction);
 
-  execute(instruction);
+  if (opcode != (uint32_t)-1) {
+      execute(instruction);
+  } else {
+      printf("Instrucción no reconocida: 0x%X\n", instruction);
+  }
 
   if (CURRENT_STATE.PC == NEXT_STATE.PC) {
       NEXT_STATE.PC += 4;
   }
 }
+
 
   /* execute one instruction here. You should use CURRENT_STATE and modify
   * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
