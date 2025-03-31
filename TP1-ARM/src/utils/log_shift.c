e040f9ab106606fd1da1955659f4b2039e1044db
anatissera <atissera@udesa.edu.ar>
2025-03-30 23:44:45 -0300

    Update log_shift.c

--- TP1-ARM/src/utils/log_shift.c
+++ TP1-ARM/src/utils/log_shift.c
@@ -1,15 +1,16 @@
 #include "log_shift.h"
 
 void lsl_lsr_imm(uint32_t instruction){   // lsl and lsr immediate
-    uint32_t rd = instruction & 0b11111;
-    uint32_t rn = (instruction & (0b11111 << 5)) >> 5;
-    uint32_t imm = (instruction & (0b111111 << 16)) >> 16;
-  
-    uint32_t mask = 0b111111 << 10;
-  
-    if (((instruction & mask) >> 10) == 0b111111){ // LSR
+    uint32_t rd = instruction & 0x1F;
+    uint32_t rn = (instruction >> 5) & 0x1F;
+    uint32_t imm = (instruction >> 16) & 0x3F;
+    uint32_t mask = 0xFC00;
+
+    uint32_t mode = (instruction & mask) >> 10;
+
+    if (mode == 0x3F) {  // 0b111111 → LSR
       NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rn] >> (64 - imm);
-    } else {                                       // LSL
+    } else {             // LSL
       NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rn] << (64 - imm);
     }
   }
